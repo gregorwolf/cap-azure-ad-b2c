f4e0a9f505674b1c9d327bc8c4a19a49f1484c43
Gregor Wolf <gregor.wolf@gmail.com>
2020-12-23 13:37:04 +0100

    Secure with helmet, add CORS

--- srv/server.js
+++ srv/server.js
@@ -1,8 +1,14 @@
-"use strict";
+"use strict"
 
-const cds = require("@sap/cds");
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const cds = require('@sap/cds')
+const helmet = require('helmet')
+const cors = require('cors')
+const proxy = require('@sap/cds-odata-v2-adapter-proxy')
 
-cds.on("bootstrap", app => app.use(proxy()));
+cds.on('bootstrap', (app) => {
+  app.use(helmet())
+  app.use(cors())
+  app.use(proxy())
+})
 
-module.exports = cds.server;
+module.exports = cds.server
