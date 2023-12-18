8f949360229a44bb0fdfd60ec600ee0b4427eb63
Gregor Wolf <gregor.wolf@gmail.com>
2023-12-18 23:12:20 +0100

    expose Products

--- srv/catalog-service.cds
+++ srv/catalog-service.cds
@@ -1,8 +1,12 @@
 using my.bookshop as my from '../db/data-model';
+using {SEPMRA_PROD_MAN as external} from './external/SEPMRA_PROD_MAN';
 
-service CatalogService @(requires : 'authenticated-user') {
+service CatalogService @(requires: 'authenticated-user') {
     @readonly
-    entity Books as projection on my.Books;
+    entity Books    as projection on my.Books;
+
+    @readonly
+    entity Products as projection on external.SEPMRA_C_PD_Product;
 
     action readSAPLogonTicket() returns String;
 }
--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -1,21 +1,25 @@
-module.exports = async function (srv){
-  
-  const biscuitService = await cds.connect.to('biscuit')
+module.exports = async function (srv) {
+  const biscuitService = await cds.connect.to("biscuit");
+  const productService = await cds.connect.to("SEPMRA_PROD_MAN");
 
-  srv.on ('readSAPLogonTicket', async req => {
+  srv.on("READ", "Products", async (req) => {
+    return productService.run(req.query);
+  });
+
+  srv.on("readSAPLogonTicket", async (req) => {
     // const biscuitServiceTransaction = biscuitService.transaction(req)
     // Results in:
     // Error: Field jku containing the URL for JWT verification not part of header.
     // at getVerificationKeyURL (/Users/gwolf/Documents/Projects/azure/cap-azure-ad-b2c/node_modules/@sap-cloud-sdk/core/dist/connectivity/scp-cf/jwt.js:353:15)
     try {
-      const response = await biscuitService.get('/sap/bc/zbiscuit?user=GWOLF')
-      return response
+      const response = await biscuitService.get("/sap/bc/zbiscuit?user=GWOLF");
+      return response;
     } catch (error) {
-      console.error("Error Message: " + error.message)
-      console.error("Stack: " + error.stack)
-      if(error.request && error.request.path) {
-        console.error("Request Path: " + error.request.path)
+      console.error("Error Message: " + error.message);
+      console.error("Stack: " + error.stack);
+      if (error.request && error.request.path) {
+        console.error("Request Path: " + error.request.path);
       }
     }
-  })
-}
+  });
+};
