2bc38854ad744072f289ad0b4255e43c68db3c04
Gregor Wolf <gregor.wolf@gmail.com>
2023-12-21 15:01:40 +0100

    MSAL 2

---
+++ app/msal-2/authConfig.js
@@ -0,0 +1,62 @@
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+const authCSWEntraID = {
+  clientId: "19702d90-5fb8-4d76-a6c9-557f55c771e8",
+  // Full directory URL, in the form of https://login.microsoftonline.com/<tenant-id>
+  authority:
+    "https://login.microsoftonline.com/6f8c8b13-6c5b-42e2-8a42-e4ef99e7ff48",
+  redirectUri: "http://localhost:6420",
+};
+
+const msalConfig = {
+  auth: authCSWEntraID,
+  cache: {
+    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case msal.LogLevel.Error:
+            console.error(message);
+            return;
+          case msal.LogLevel.Info:
+            console.info(message);
+            return;
+          case msal.LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case msal.LogLevel.Warning:
+            console.warn(message);
+            return;
+        }
+      },
+    },
+  },
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit:
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+const loginRequest = {
+  scopes: ["User.Read"],
+};
+
+/**
+ * Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+const tokenRequest = {
+  scopes: ["User.Read", "Mail.Read"],
+  forceRefresh: false, // Set this to "true" to skip a cached token and go to the server to get a new token
+};
